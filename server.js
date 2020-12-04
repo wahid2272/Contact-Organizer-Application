d108425f4e650dd76bd3a5dd860618f708c99eec
Wahiduzzaman Siddique <wahid2272@gmail.com>
2020-12-04 13:52:52 +0200

    Configure database and connect with mongoDB

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
+// Connect Database here
+connectDB();
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome'}))
 
