8b0983727296a9f163cce849e579e9f4ec263cd8
Wahiduzzaman Siddique <wahid2272@gmail.com>
2020-12-04 16:17:51 +0200

    User password is hashed and user registration data is routed to mongoDB server

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
@@ -14,13 +15,36 @@ router.post('/', [
   body('email', 'Please include a valid email').isEmail(),
   body('password', 'Password needs to be 6 or more chracters').isLength({ min: 6 })
 ], 
-(req, res) => {
+async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  res.send('test')
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email: email });
+    if(user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = new User({
+      name: name,
+      email: email,
+      password: password
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+  }
+  catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
