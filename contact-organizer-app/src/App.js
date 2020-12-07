2308af354a11f8bda630f780d13da5f4f3c17743
IbraheemLanre <s2001108@edu.bc.fi>
2020-12-07 15:30:47 +0200

    created login templates and functionality

--- contact-organizer-app/src/App.js
+++ contact-organizer-app/src/App.js
@@ -1,9 +1,13 @@
+import React from "react"
 import './App.css';
+import Login from './component/login/Login';
 
 function App() {
   return (
     <div className="App">
       This is Login Page!
+
+      <Login/>
     </div>
   );
 }
