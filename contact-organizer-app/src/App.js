bc726b39cf09087870f50e937e6ea722df554f8d
IbraheemLanre <s2001108@edu.bc.fi>
2020-12-09 12:35:19 +0200

    added login page functionality

--- contact-organizer-app/src/App.js
+++ contact-organizer-app/src/App.js
@@ -1,13 +1,38 @@
-import React from "react"
-import './App.css';
-import Login from './component/login/Login';
+import React from "react";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+import "./App.css";
+
+import Login from "./component/login/Login";
+import Dashboard from "./component/dashboard/Dashboard";
+import Home from "./component/home/Home";
 
 function App() {
   return (
     <div className="App">
-      This is Login Page!
-
-      <Login/>
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">
+              Home
+            </NavLink>
+            <NavLink activeClassName="active" to="/login">
+              Login
+            </NavLink>
+            <small>(Access without token only)</small>
+            <NavLink activeClassName="active" to="/dashboard">
+              Dashboard
+            </NavLink>
+            <small>(Access with token only)</small>
+          </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
