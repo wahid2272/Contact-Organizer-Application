fe863f2663648413f0d4f4f98abdd86a3d1e0420
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2020-12-15 21:52:41 +0200

    Backend styling (#9)
    * [Landing Page] - added footer component
    
    * [Landing Page & Styling] - added logo, hero section, image and content and changed background colour
    
    * [Styling] - installed material ui library and styled input buttons
    
    * [Styling] - refactored login page styling

--- contact-organizer-app/src/components/Login.js
+++ contact-organizer-app/src/components/Login.js
@@ -2,6 +2,8 @@ import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '.././services/alert/AlertContext';
 import AuthContext from '.././services/authentication/AuthenticationContext';
 
+
+
 const Login = (props) => {
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
@@ -41,41 +43,53 @@ const Login = (props) => {
       });
     }
   };
+  const image = 'https://source.unsplash.com/700x500/?phonebook';
 
   return (
-    <div className='form-container'>
-      <h1>
-        Account <span className='text-primary'>Login</span>
-      </h1>
-      <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label htmlFor='email'>Email Address</label>
+    <div className="wrapper">
+      <div className="hero">
+        <div className="hero-content">
+            <p>
+              Contact Organizer helps you <br></br> to manage information of the <br></br> <strong><span>people that matter to you.</span></strong>
+            </p>
+          </div>
+            <div className={'hero-img'} style={{backgroundImage: `url(${image})`}}/>
+      </div>
+      <div className='form-container'>
+      
+        <h1>
+          Account <span className='text-primary'>Login</span>
+        </h1>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='email'>Email Address</label>
+            <input
+              id='email'
+              type='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              id='password'
+              type='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              required
+            />
+          </div>
           <input
-            id='email'
-            type='email'
-            name='email'
-            value={email}
-            onChange={onChange}
-            required
+            type='submit'
+            value='Login'
+            className='btn btn-primary btn-block'
           />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password</label>
-          <input
-            id='password'
-            type='password'
-            name='password'
-            value={password}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <input
-          type='submit'
-          value='Login'
-          className='btn btn-primary btn-block'
-        />
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
--- contact-organizer-app/src/components/Navbar.js
+++ contact-organizer-app/src/components/Navbar.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import AuthContext from '../services/authentication/AuthenticationContext';
 import ContactContext from '../services/contactState/ContactContext';
+import logo from './logo.svg';
+
+import Button from '@material-ui/core/Button';
 
 const Navbar = ({ title, icon}) => {
   const authContext = useContext(AuthContext);
@@ -35,22 +38,25 @@ const Navbar = ({ title, icon}) => {
 
   const guestLinks = (
     <>
+    <Button variant="contained" color="secondary-color">
       <li>
         <Link to='/register'>Register</Link>
       </li>
+    </Button>
+    <Button variant="contained">
       <li>
         <Link to='/login'>Login</Link>
       </li>
+    </Button>
     </>
   );
 
   return (
     <div className='navbar bg-primary'>
-      <h1>
-        <Link to='/'>
-          <i className={icon} /> {title}
-        </Link>
-      </h1>
+      
+      <div className="header-logo">
+        <Link to='/'><img src={logo} alt="logo" /></Link>
+      </div>
       <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
     </div>
   );
