a24aad17d08f2c2102e99fc4982b655835e20487
Wahiduzzaman Siddique <wahid2272@gmail.com>
2020-12-16 01:07:06 +0200

    Prepared for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Connect Database here
 connectDB();
@@ -14,6 +15,13 @@ app.use(express.json({ extended: false}));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Production
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('contact-organizer-app/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'contact-organizer-app', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
