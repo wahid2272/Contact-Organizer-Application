bdbddd778d107b67b8f9bd732ec67828aa4376dd
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-03-01 23:28:34 +0200

    Bug fixed: logged-in user's saved contacts are visible now

--- contact-organizer-app/src/services/contactState/ContactReducer.js
+++ contact-organizer-app/src/services/contactState/ContactReducer.js
@@ -1,15 +1,15 @@
 import {
-  GET_CONTACTS,
   ADD_CONTACT,
-  DELETE_CONTACT,
-  SET_CURRENT,
+  CLEAR_CONTACTS,
   CLEAR_CURRENT,
-  UPDATE_CONTACT,
-  FILTER_CONTACTS,
   CLEAR_FILTER,
   CONTACT_ERROR,
-  CLEAR_CONTACTS
-} from '../types';
+  DELETE_CONTACT,
+  FILTER_CONTACTS,
+  GET_CONTACTS,
+  SET_CURRENT,
+  UPDATE_CONTACT,
+} from "../types";
 
 // eslint-disable-next-line
 export default (state, action) => {
@@ -17,26 +17,30 @@ export default (state, action) => {
     case GET_CONTACTS:
       return {
         ...state,
-        conatcts: action.payload,
-        loading: false
-      }
+        contacts: action.payload,
+        loading: false,
+      };
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [ action.payload, ...state.contacts ],
-        loading: false
+        contacts: [action.payload, ...state.contacts],
+        loading: false,
       };
     case UPDATE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.map(contact => contact._id ===action.payload._id ? action.payload :contact ),
-        loading: false
+        contacts: state.contacts.map((contact) =>
+          contact._id === action.payload._id ? action.payload : contact
+        ),
+        loading: false,
       };
     case DELETE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.filter(contact => contact._id !== action.payload),
-        loading: false
+        contacts: state.contacts.filter(
+          (contact) => contact._id !== action.payload
+        ),
+        loading: false,
       };
     case CLEAR_CONTACTS:
       return {
@@ -44,37 +48,40 @@ export default (state, action) => {
         contacts: null,
         filtered: null,
         error: null,
-        current: null
-      }
+        current: null,
+      };
     case SET_CURRENT:
       return {
         ...state,
-        current: action.payload
+        current: action.payload,
       };
     case CLEAR_CURRENT:
       return {
         ...state,
-        current: null
+        current: null,
       };
     case FILTER_CONTACTS:
       return {
         ...state,
-        filtered: state.contacts.filter(contact => {
-          const regularexpression = new RegExp(`${action.payload}`, 'gi');
-          return contact.name.match(regularexpression) || contact.email.match(regularexpression);
-        })
+        filtered: state.contacts.filter((contact) => {
+          const regularexpression = new RegExp(`${action.payload}`, "gi");
+          return (
+            contact.name.match(regularexpression) ||
+            contact.email.match(regularexpression)
+          );
+        }),
       };
     case CLEAR_FILTER:
       return {
         ...state,
-        filtered: null
+        filtered: null,
       };
     case CONTACT_ERROR:
       return {
         ...state,
-        error: action.payload
-      }
+        error: action.payload,
+      };
     default:
       return state;
   }
-}
+};
